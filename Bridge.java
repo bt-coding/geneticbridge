64799a9156d1c4ef145393c82e729d1b542c27f3
Tristan Lonsway <tblonswa@syr.edu>
2019-12-05 09:03:27 -0500

    Add node searching optimization

--- Bridge.java
+++ Bridge.java
@@ -3,20 +3,26 @@ import java.util.*;
 public class Bridge {
     ArrayList<Node> nodelist;
     ArrayList<ArrayList<Node>> members;
-    
     public Bridge() {
         nodelist = new ArrayList<Node>();
         members = new ArrayList<ArrayList<Node>>();
     }
-     
     public void addNode(Node n) {
         nodelist.add(n);
     }
     public void addMember(Node n1, Node n2, boolean externalnodes) {
         //set externalnodes to true if the argument nodes are not direct references to the existing nodelist
         if (externalnodes) {
-            
-            
+            for(Node node : nodelist) {
+                double nodex = node.getX();
+                double nodey = node.getY();
+                if (nodex==n1.getX() && nodey==n1.getY()) {
+                    n1 = node;
+                }
+                if (nodex==n2.getX() && nodey==n2.getY()) {
+                    n2 = node;
+                }
+            }
         }
         ArrayList<Node> temp = new ArrayList<Node>();
         temp.add(n1);
@@ -29,9 +35,4 @@ public class Bridge {
     public ArrayList<ArrayList<Node>> getMembers() {
         return members;
     }
-    
-    
-    
-    
-    
 }
